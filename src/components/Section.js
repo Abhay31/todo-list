9ec76a6d2649428586d5f66f17384f42315a9a50
Abhay31 <abhaysh3101@gmail.com>
2025-03-01 14:19:05 +0530

    new_chngs_Abhay

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,10 +3,10 @@ import Task from './Task';
 
 const Section = ({ title, tasks, onMove, onDelete }) => {
   return (
-    <div className="w-full md:w-1/3 p-4"> {/* Adjust width for responsiveness */}
+    <div className="w-full md:w-1/3 p-4"> 
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       {tasks.map(task => (
-        <Task key={task.id} task={task} onMove={onMove} onDelete={onDelete} /> // Pass onDelete prop to Task
+        <Task key={task.id} task={task} onMove={onMove} onDelete={onDelete} /> 
       ))}
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,29 +3,29 @@ import { FaTrashAlt } from 'react-icons/fa';
 
 const Task = ({ task, onMove, onDelete }) => {
   const handleDelete = () => {
-    onDelete(task.id); // Call onDelete with task ID
+    onDelete(task.id);
   };
 
   return (
     <div className="bg-white p-4 rounded shadow mb-4">
       <div className="flex justify-between items-center">
-        <div className="w-full md:w-4/5"> {/* Adjust width for responsiveness */}
+        <div className="w-full md:w-4/5">
           <h3 className="font-bold text-lg">{task.title}</h3>
           {task.description && <p className="text-gray-600">{task.description}</p>}
           {task.status === 'Completed' && <p className="text-gray-500 text-sm mt-2">Completed at: {task.completedAt}</p>}
         </div>
-        <div className="flex items-center w-full md:w-1/5 justify-end"> {/* Adjust width for responsiveness */}
+        <div className="flex items-center w-full md:w-1/5 justify-end">
           {task.status !== 'Completed' && (
-            <button 
+            <button
               className="mt-2 md:mt-0 bg-blue-500 text-white px-4 py-2 rounded mr-2"
               onClick={() => onMove(task.id)}
             >
               {task.status === 'Pending' ? 'Start' : 'Complete'}
             </button>
           )}
-          <button 
+          <button
             className="mt-2 md:mt-0 bg-red-500 text-white px-4 py-2 rounded"
-            onClick={handleDelete} // Call handleDelete on click
+            onClick={handleDelete}
           >
             <FaTrashAlt />
           </button>
